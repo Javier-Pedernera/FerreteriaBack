31383ff94fce5fdeaef2c6cd5f2ceb40a51073a3
Juan Javier Pedernera <102430306+Javier-Pedernera@users.noreply.github.com>
2025-07-06 09:50:58 -0300

    calculo precio final

--- app/models/producto.py
+++ app/models/producto.py
@@ -51,14 +51,12 @@ class Producto(db.Model):
         except Exception:
             return None
     
-    def precio_por_unidad(self):
+    def calcular_precio_final(self):
         try:
-            if self.presentacion_cantidad and self.presentacion_cantidad > 0:
-                valor_real = float(self.precio_final) / self.presentacion_cantidad
-                # Redondear hacia arriba al múltiplo de 5 más cercano
-                redondeado = math.ceil(valor_real / 5) * 5
-                return redondeado
-            return None
+            porcentaje = self.porcentaje_ganancia if self.porcentaje_ganancia_personalizado else self.proveedor.porcentaje_ganancia
+            base = float(self.precio_ars) * (1 + porcentaje / 100)
+            redondeado = math.ceil(base / 5) * 5  # Redondea hacia arriba al múltiplo de 5
+            return redondeado
         except Exception:
             return None
     
