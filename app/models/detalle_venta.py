0984170374a1f656e809bc8f7c390be3b1abbb53
Juan Javier Pedernera <102430306+Javier-Pedernera@users.noreply.github.com>
2025-12-18 22:59:35 -0300

    cambios en detalle venta

--- app/models/detalle_venta.py
+++ app/models/detalle_venta.py
@@ -6,7 +6,7 @@ class DetalleVenta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     venta_id = db.Column(db.Integer, db.ForeignKey('ventas.id'), nullable=False)
     producto_id = db.Column(db.Integer, db.ForeignKey('productos.id'), nullable=False)
-
+    precio_costo = db.Column(db.Numeric(12, 2), nullable=True)
     cantidad = db.Column(db.Integer, nullable=False)
     precio_unitario = db.Column(db.Numeric, nullable=False)
 
@@ -20,7 +20,7 @@ class DetalleVenta(db.Model):
         "producto_id": self.producto_id,
         "producto": getattr(producto, "nombre", None),
         "cod_interno": getattr(producto, "cod_interno", None),
-        "precio_costo": getattr(producto, "precio_ars", None),
+        "precio_costo": str(self.precio_costo),
         "presentacion_cantidad": getattr(producto, "presentacion_cantidad", None),
         "unidad_medida": getattr(producto, "unidad_medida.codigo", None) if producto and producto.unidad_medida else None,
         "disponibles": getattr(producto, "disponibles", None),
